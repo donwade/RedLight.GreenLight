e0f3491064365312f17449015d2d7fdf0cfbe4cb
Don Wade <don_wade@rogers.com>
2025-08-06 14:59:21 -0400

    remove learning and reporting from redlightgreenlight

--- SquidGames/locate.h
+++ SquidGames/locate.h
@@ -1,3 +1,6 @@
+#include <M5Unified.h>
+#include <TinyGPS++.h>
+
 extern void setup_locate(void);
 
 typedef struct  
@@ -9,6 +12,8 @@ typedef struct
          char crossStreet[30];
 } GPS_ENTRY2;
 
+extern TinyGPSPlus gps;
+
 extern GPS_ENTRY2 *closestCam;
 extern GPS_ENTRY2 *nextClosestCam;
 
@@ -16,4 +21,27 @@ extern GPS_ENTRY2 *nextClosestCam;
 extern int findNearestCamera(float vehicleLat, float vehicleLng);
 extern int32_t writeToSD(char* filename = "backup.db" );
 
+typedef struct gpsLocation { double lng; double lat; };
+
+typedef struct gpsMisc 
+{
+	float speed;
+	const char *cardinal;
+	
+	float qual;
+	const char  *cQuality;
+	
+	float Kmph;
+	float course;		//direction in float degrees
+	
+	uint8_t hour;
+	uint8_t minute;
+	uint8_t second;
+};
+	
+extern gpsLocation iLocation;
+extern gpsMisc     iMisc;
+extern gpsLocation cameraLocation;
+extern gpsLocation endLocation;
+extern gpsLocation gpsAverage;
 
