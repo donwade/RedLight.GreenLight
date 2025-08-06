72a5be29e1d2d454f5ccd9d74ae086209ccc4f61
Don Wade <don_wade@rogers.com>
2025-08-06 15:00:08 -0400

    reportingView.cpp and learningView are separated

---
+++ SquidGames/learningView.cpp
@@ -0,0 +1,88 @@
+#include "locate.h"
+#include "viewController.h"
+
+extern void * reportingMode(BUTTON_EVENT x);
+
+#define LINE Serial.printf("%s:%d\n", __FUNCTION__, __LINE__)
+
+void * learningMode(BUTTON_EVENT some_key)
+{
+	int dist;
+	int course;
+	const char *dir;
+	static KEY_STATE here;
+	
+#if 0
+	if (iMisc.Kmph > 5)
+		xprintf(2, "%3d %s", veh_course, veh_cardinal);
+	else
+		xprintf(2, "%2d/%2d/%4d S=%2d", gps.date.day(), gps.date.month(), 
+				gps.date.year(), gps.satellites.value());
+#endif	
+	cprintf(_WHITE, 0, "NOW    LA=%+10.7f", gpsAverage.lat);
+	cprintf(_WHITE, 1, "NOW    LN=%+10.7f", gpsAverage.lng);
+	cprintf(_RED, 2,   "CAMERA LA=%+9.7f", cameraLocation.lat);
+	cprintf(_RED ,3,   "CAMERA LO=%+9.7f", cameraLocation.lng);
+	cprintf(_GREEN, 4, "AWAY   LA=%+9.7f", endLocation.lat);
+	cprintf(_GREEN, 5, "AWAY   LO=%+9.7f", endLocation.lng);
+
+	dist = gps.distanceBetween(cameraLocation.lat, cameraLocation.lng, endLocation.lat, endLocation.lng);
+	course = (int)gps.courseTo(cameraLocation.lat, cameraLocation.lng, endLocation.lat, endLocation.lng);
+	dir = gps.cardinal(course);
+	
+	cprintf(_YELLOW, 6, "course = %d dir=%3s", course, dir);
+	
+	// all display updates done ... just keys left
+	if (some_key == DISPLAY_REFRESH) return (void*) learningMode;
+	
+	Serial.printf("handled key %d\n", some_key);
+
+	switch (some_key)
+	{
+		case BUTTON_INIT:
+			lfillRect(0,0, 50, 50, _BLUE);
+			threeButtonText("AWAY", "SAVE", "CAMERA");
+			cprintf(_RED,	2, "LA=%+9.7f", gpsAverage.lat);
+			cprintf(_RED ,	3, "LO=%+9.7f", gpsAverage.lng);
+			cprintf(_GREEN, 4, "LA=%+9.7f", gpsAverage.lat);
+			cprintf(_GREEN, 5, "LO=%+9.7f", gpsAverage.lng);
+			cprintf(_ORANGE,6, "CAMERA or AWAY");
+		break;	
+			
+		case LBUTTON_UP:
+		case LBUTTON_DN:
+			if (some_key == LBUTTON_DN)
+			{
+				endLocation = gpsAverage;
+				colourBarX(_GREEN, 10);
+				cprintf(_ORANGE, 6, "NEXT CAMERA or SAVE");
+			}
+			
+		break;
+
+		case RBUTTON_UP:
+		case RBUTTON_DN:
+			if (some_key == RBUTTON_DN)
+			{
+				cameraLocation = gpsAverage;
+				colourBarX(_RED, 10);
+				cprintf(_ORANGE, 6, "NEXT AWAY or SAVE");
+			}
+
+		break;
+
+		case MBUTTON_DN:
+		case MBUTTON_UP:
+			
+			if (some_key == MBUTTON_DN)
+			{
+				colourBarX(_YELLOW, 10);
+				return (void*) reportingMode;
+			}			
+			break;
+		break;
+
+	}
+	return (void*) learningMode;
+}
+
---
+++ SquidGames/reportingView.cpp
@@ -0,0 +1,118 @@
+#include "locate.h"
+#include "viewController.h"
+
+#define LINE Serial.printf("%s:%d\n", __FUNCTION__, __LINE__)
+extern void * learningMode(BUTTON_EVENT x);
+
+void * reportingMode(BUTTON_EVENT some_key)
+{
+	int dist;
+	int course;
+	const char *dir;
+	static KEY_STATE here;
+	const char *cardinal;
+
+
+	findNearestCamera(iLocation.lat, iLocation.lng);
+	
+	course = (int)gps.courseTo(iLocation.lat, iLocation.lng, closestCam->lat, closestCam->lng);
+	cardinal = gps.cardinal(course);
+
+	dist = (int) gps.distanceBetween(iLocation.lat, iLocation.lng, closestCam->lat, closestCam->lng);
+	
+	cprintf(_WHITE, 0, "%s", closestCam->onStreet);
+	cprintf(_WHITE, 1, "%s",  closestCam->crossStreet);
+	cprintf(dist > 100 ? _GREEN : _RED, 2, "DIST=%4d m %3d %s", dist, course, cardinal);
+
+	xprintf(3, "%VEH=%3d kph Qual=%s", (int)iMisc.Kmph, iMisc.cQuality);
+
+	cprintf(_GREEN, 4, "NOW LA=%+9.7f", gpsAverage.lat);
+	cprintf(_GREEN, 5, "NOW LO=%+9.7f", gpsAverage.lng);
+
+    bool isCharging = M5.Power.isCharging();
+    int vol_per = M5.Power.getBatteryLevel();
+    int vol = M5.Power.getBatteryVoltage();
+    int cur = M5.Power.getBatteryCurrent();
+	cprintf(_YELLOW, 6, "%3.1fv %d%% %dmA %s", 
+						(float)vol/1000.,
+						vol_per,
+						cur,
+						isCharging ? "CHG":"DIS");
+	
+	// all display updates done ... just keys left
+	if (some_key == DISPLAY_REFRESH) return (void*) reportingMode;
+
+	Serial.printf("handled key %d\n", some_key);
+
+	switch (some_key)
+	{
+		case BUTTON_INIT:
+			lfillRect(0,0, 50, 50, _RED);
+			threeButtonText("QUIET", "OK", "BYTEME");
+			
+			cprintf(_RED,	2, "TODO        ");
+			cprintf(_RED ,	3, "TODO        ");
+			cprintf(_GREEN, 4, "TODO        ");
+			cprintf(_GREEN, 5, "TODO        ");
+			cprintf(_ORANGE,6, "TODO        ");
+			
+		break;	
+			
+		case LBUTTON_UP:
+		case LBUTTON_DN:
+
+			if (some_key == LBUTTON_DN)
+			{
+				colourBarX(_CYAN, 10);
+				cprintf(_ORANGE, 6, "TODO LEFT");
+			}
+			
+		break;
+
+		case RBUTTON_UP:
+		case RBUTTON_DN:
+
+			if (some_key == RBUTTON_DN)
+			{
+				colourBarX(_BLUE, 10);
+				cprintf(_BLUE, 6, "TODO RIGHT");
+			}
+
+		break;
+
+		case MBUTTON_DN:
+		case MBUTTON_UP:
+
+			if (some_key == MBUTTON_DN)
+			{
+				colourBarX(_PINK, 10);
+				return (void*) learningMode;
+			}			
+			
+		break;
+
+	}
+
+/*
+	What is HDOP 
+	< 1 Ideal
+
+	1-2 Excellent
+	Highest possible confidence level to be used for applications demanding the highest possible precision at all times.
+	At this confidence level, positional measurements are considered accurate enough to meet all but the most sensitive applications.
+
+	2-5 Good
+
+	Represents a level that marks the minimum appropriate for making accurate decisions. Positional measurements could be used to make reliable in-route navigation suggestions to the user.
+	Positional measurements could be used for calculations, but the fix quality could still be improved. A more open view of the sky is
+
+	5-10 Moderate
+
+	10-20 Fair
+	Represents a low confidence level. Positional measurements should be discarded or used only to indicate a very rough estimate
+
+	>20 Poor At this level, measurements should be discarded
+*/
+
+	return (void *) reportingMode;
+}	
